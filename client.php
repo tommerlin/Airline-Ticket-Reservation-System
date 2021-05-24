6bb6ff60fc65b83f832fdb71e43a9d83c3e2b2cb
Preeti Manoj <preeti@dexlock.com>
2021-05-24 23:45:07 +0530

    db setup

--- client.php
+++ client.php
@@ -20,8 +20,8 @@
                 </button>
             </div>
         </div>
-        
-        <form action="user_db.php" method="post" class="d-flex flex-column bd-highlight mb-3 w-25">
+        <div class="client-details">
+        <form action="user_db.php" method="post" >
             <label for="fname"><b>First Name</b></label>
             <input type="text" placeholder="First Name" name="fname" id="fname" required class="mb-2">
 
@@ -31,37 +31,41 @@
             <label for="phno"><b>Phone Number</b></label>
             <input type="number" placeholder="Phone Number" name="phno" id="phno" required class="mb-2">
 
-            
+            <br/>
             <button type="button" class="btn btn-primary">Sign In</button>
         </form>
 
         <form action="user_db.php" method="post">
 
             <label for="sdate"><b>Find total hours travelled between</b></label>
-            <input type="date" placeholder="Start Date" name="sdate" id="sdate" required>
+            <input type="date" placeholder="Start Date" name="sdate" id="sdate" required class="mb-2">
 
-            <label for="edate"><b>and</b></label>
-            <input type="date" placeholder="End Date" name="edate" id="edate" required>
+           <br/>
+            <input type="date" placeholder="End Date" name="edate" id="edate" required class="mb-2">
+            <br/>
+            <button type="button" class="btn btn-primary mb-2" >Submit</button>
+            <p>Total hours traveled: </p>
+            
+        </form>
+       
+        
+        <form action="user_db.php" method="post">
+        <button type="button" class="btn btn-primary">Get travel history</button>
+        </form>
 
+        <form action="user_db.php" method="post">
+                <label for="origin"><b>Origin</b></label>
+                <input type="text" placeholder="Origin" name="origin" id="origin" required class="mb-2">
+                <label for="dest"><b>Destination</b></label>
+                <input type="text" placeholder="Destination" name="dest" id="dest" required class="mb-2">
+                <label for="origin"><b>Travel Date</b></label>
+                <input type="date" placeholder="Travel Date" name="tdate" id="tdate" required class="mb-2">
+                <label for="dest"><b>Return Date</b></label>
+                <input type="date" placeholder="Return Date" name="rdate" id="rdate" required class="mb-2">
+                <br/>
             <button type="button" class="btn btn-primary">Submit</button>
         </form>
-        <p>Total hours traveled: </p>
-        <form action="user_db.php" method="post">
-        <form action="user_db.php" method="post">
-
-
-<button type="button" class="btn btn-primary">Get travel history</button>
-</form>
-
-<form action="user_db.php" method="post">
-
-        <label for="origin"><b>Origin</b></label>
-                <input type="text" placeholder="Origin" name="origin" id="origin" required>
-
-                <label for="dest"><b>Destination</b></label>
-                <input type="text" placeholder="Destination" name="dest" id="dest" required>
-            <button type="button" class="btn btn-primary">Submit</button>
-            </form>
+        </div>
     </div>
    
     
