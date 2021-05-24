91e68d724b45c86867e4767895ec8d064219156d
Preeti Manoj <preeti@dexlock.com>
2021-05-24 18:50:27 +0530

    styling

--- airline.php
+++ airline.php
@@ -4,6 +4,22 @@
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
+<div class="main-container">
+        <div class="heading">
+            <h3>Airline Booking System</h3>
+            <div class="buttons">
+                <button type="submit" class="btn btn-warning">
+                    <a href="data.php">Add new Data</a>
+                </button>
+                <button type="submit" class="btn btn-warning">
+                    <a href="airline.php">Airline Details</a>
+                </button>
+                <button type="submit" class="btn btn-warning">
+                <a href="client.php">User Details</a>
+                </button>
+            </div>
+        </div>
+  
     
     <div class="main-container m-3 d-flex justify-content-around m-3">
         <!-- <div>
@@ -87,7 +103,7 @@
            <label for="hrair"><b>Airline Name</b></label>
            <input type="text" placeholder="Airline Name" name="hrair" id="hrair" required class="mb-2">
 
-           <label for="lname"><b>Total hours by aircraft</b></label>
+           <label for="lname"><b>Aircraft Name</b></label>
             <input type="text" placeholder="Aircraft Name" name="lname" id="lname" required class="mb-2">
 
            <label for="sdate"><b>Start Date</b></label>
@@ -115,7 +131,7 @@
 
 
     </div>
-
+    </div>
     
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
--- data.php
+++ data.php
@@ -2,12 +2,28 @@
 <head>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> 
 <link rel="shortcut icon" href="#">
+<link rel="stylesheet" href="style.css">
 </head>
 <body>
+<div class="main-container">
+        <div class="heading">
+            <h3>Airline Booking System</h3>
+            <div class="buttons">
+                <button type="submit" class="btn btn-warning">
+                    <a href="data.php">Add new Data</a>
+                </button>
+                <button type="submit" class="btn btn-warning">
+                    <a href="airline.php">Airline Details</a>
+                </button>
+                <button type="submit" class="btn btn-warning">
+                <a href="client.php">User Details</a>
+                </button>
+            </div>
+        </div>
     
-    <div class="d-flex justify-content-between m-3" >
-        <div class="w-25">
-            <h3> Insert Airline </h3>
+    <div class="data-container" >
+        <div class="data-form">
+            <h3> Create New Airline </h3>
             <form action="user_db.php?id=1" method="post" >
                 <div class="d-flex flex-column bd-highlight mb-3">
                 <label for="airline"><b>Airline Name</b></label>
@@ -24,8 +40,8 @@
             </form>
             
         </div>
-        <div class="w-25">
-            <h3> Insert Aircraft </h3>
+        <div class="data-form">
+            <h3> Create New Aircraft </h3>
             <form action="user_db.php?id=2" method="post">
             <div class="d-flex flex-column bd-highlight mb-3">
                 <label for="aircid"><b>Aircraft ID</b></label>
@@ -50,10 +66,18 @@
                 <button type="submit" class="btn btn-primary">Register</button>
             </form>
         </div>
-        <div class="w-25">
-            <h3> Insert User </h3>
+        <div class="data-form">
+            <h3> Create New User </h3>
             <form action="user_db.php?id=3" method="post">
             <div class="d-flex flex-column bd-highlight mb-3">
+
+                <label for="origin"><b>Origin</b></label>
+                <input type="text" placeholder="Origin" name="origin" id="origin" required>
+
+                <label for="dest"><b>Destination</b></label>
+                <input type="text" placeholder="Destination" name="dest" id="dest" required>
+
+
                 <label for="fname"><b>First Name</b></label>
                 <input type="text" placeholder="First Name" name="fname" id="fname" required>
 
@@ -70,12 +94,30 @@
             </form>
         </div>
         
-       
+        <div class="data-form">
+            <h3> Create New Booking </h3>
+            <form action="user_db.php?id=4" method="post">
+            <div class="d-flex flex-column bd-highlight mb-3">
+                <label for="fname"><b>First Name</b></label>
+                <input type="text" placeholder="First Name" name="fname" id="fname" required>
+
+                <label for="lname"><b>Last Name</b></label>
+                <input type="text" placeholder="Last Name" name="lname" id="lname" required>
+
+                <label for="phno"><b>Phone Number</b></label>
+                <input type="number" placeholder="Phone Number" name="phno" id="phno" required>
+
+                <label for="email"><b>Email</b></label>
+                <input type="text" placeholder="Email" name="email" id="email" required>
+                </div>
+                <button type="button" class="btn btn-primary">Register</button>
+            </form>
+        </div>
        
 
     </div>
 
-    
+    </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
